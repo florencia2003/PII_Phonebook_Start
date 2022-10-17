5543af005072153f935574d3d0c0ba0b1ad60863
sebamart12 <93790958+sebamart12@users.noreply.github.com>
2022-10-17 09:20:07 -0300

    Twitter

---
+++ src/Library/Messagetwitter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    //Sebatian Martinez
+    public class Messagetwitter : IMessageChannel
+    {
+
+       public void Send(Message message)
+       {
+            
+       }
+
+
+    }
+}
